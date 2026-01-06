33dcaf93d2686be82d1656f5891140b334bfbf17
edyinvesti <seu-email-aqui@exemplo.com>
2026-01-06 18:52:51 -0300

    Fix: Criando arquivo de conexão supabaseClient

---
+++ src/supabaseClient.ts
@@ -0,0 +1,8 @@
+﻿import { createClient } from '@supabase/supabase-client'
+
+// Substitua pelas suas chaves se forem diferentes, 
+// mas estas costumam ser as padrão do seu projeto atual
+const supabaseUrl = 'https://onpbtvpspgvgtstfzxue.supabase.co'
+const supabaseAnonKey = 'SUA_CHAVE_ANON_AQUI' 
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
